96dce437deb166333c9749a4f8e7cdfa898a2e6c
Khrystyna Mukha <khrystynamukha29@gmail.com>
2024-08-15 22:48:24 +0300

    lesson 6 + homework

--- js/script.js
+++ js/script.js
@@ -1,63 +1,140 @@
-//  -- 1 --
-// У вас є масив об’єктів fruts, і в кожному з них є name
-// Напишіть код, який перетворює їх в масив імен.
-/*
-const fruts = [
-   { id: 0, name: "Apple" },
-   { id: 1, name: "Tomat" },
-   { id: 2, name: "Cherry" },
-   { id: 3, name: "Orange" },
-];
+/*class Bank {
+    static type = "privat";
 
-let result = fruts.map(fruts => fruts.name);
-console.log(result);*/
-
-//  -- 2 --
-//Виведіть парні числа від 2 до 10, використовуючи цикл for.
-/*
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-for (let num of arr) {
-    if (num % 2 == 0) {
-        console.log(num);
+    constructor(options) {
+        this.summ = options.summ;
+        this.month = options.month;
+        this.p = options.p;
     }
-}*/
 
-//  -- 3 --
-//Замініть цикл "for" на "while"
-// for (let i = 0; i < 5; i++) {
-//     console.lpg( `цифра ${i}!` );
-//   }
-/*
-let i = 0;
-while (i < 5) {
-    i++;
-    console.log(`number ${i}!`);
+    credit() {
+        return console.log("i am privat");
+    }
 }
+
+const userBank = new Bank({
+    summ: 12000,
+    month: 5,
+    p: 1220,
+});
+
+userBank.credit();
+console.log(user)
+
+
+class NewBank extends Bank {
+    constructor(options){
+        super(options);
+        this.card = options.card;
+    }
+
+    credit () {
+        super.credit();
+        return console.log("i am aval");
+    }
+}
+
+const aval = new NewBank({
+    summ: 12000,
+    month: 5,
+    p: 1220,
+    card: true,
+})
+
+console.log(aval.credit());
 */
 
-//  -- 4 --
-//Напишіть цикл, який пропонує prompt ввести число більше за 100.
-//Якщо відвідувач введе менше число – попросити ввести ще раз, і так далі.
-//Цикл повинен запитувати число доти, доки відвідувач не введе число,
-// більше за 100, або не скасує ввід/введе порожній рядок.
+
 /*
-let num;
+class User {
+    constructor(props) {
+        this.name = props.name;
+    }
 
-do {
-    num = prompt("enter number greater than 100");
-} while(num <= 100 && num !== null)
+    firstName = "";
+    lastName = "";
+    age = "";
+    city = "";
+
+    set name(newName) {
+        const nameRow = newName.split(" ");
+        this.firstName = nameRow[0];
+        this.lastName = nameRow[1];
+    }
+
+    get name() {
+        return ` First name:  ${this.firstName} Last name: ${this.lastName}`;
+    }
+}
+
+const kris = new User ({
+    name: "Khrystyna Mukha",
+})
+
+const mary = new User ({
+    name: "Mariia Kit",
+})
+
+console.log(kris);
+console.log(mary);
 */
 
-//  -- 5 --
-// Вирахуйте середній вік
+/*
+const Manager = function (name, sales) {
+    this.name = name;
+    this.sales = sales;    
+}
 
- const girls = [
-   { age: 23, name: "Оля" },
-   { age: 29, name: "Аня" },
-   { age: 10, name: "Юля" },
-   { age: 20, name: "Катя" },
- ];
+const ann = new Manager("Anna", 5);
+const mary = new Manager("Mariia", 7);
 
- let sum = girls.reduce((summ, item) => summ + item.age, 0) / girls.length;
- console.log(sum);  
+Manager.prototype.sale = function() {
+    this.sales += 1;
+}
+
+console.dir(Manager);
+*/
+/*
+class CoffeMachine {
+    _water = 0;
+    #waterLimit = 500;
+    constructor(power) {
+        this.power = power;
+    }
+
+    set waterAmount (value) {
+        if (value < 0) {
+            value = 0;
+        }
+        this._water = value;
+    }
+}
+
+let coffeMachine = new CoffeMachine (100);
+
+
+coffeMachine.waterAmount = -20;
+console.log(coffeMachine);
+console.log()*/
+
+class Abonent {
+    constructor(name, number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    set (name, number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    get() {
+        return `name: ${this.name} number: ${this.number}`;
+    }
+}
+
+const user1 = new Abonent("kris", "262674");
+const user2 = new Abonent("ira", "78664");
+const user3 = new Abonent("mary", "79645");
+
+console.log(user1.get());
